aab208f6e6c67cd1e3696b8ad487c4e9e5781bfc
Zenitude <evan.damas.2003@gmail.com>
2023-10-17 14:56:15 +0200

    Suppression Couche_IHM

--- SushiGo/UI_Layer/HomeView.xaml.cs
+++ SushiGo/UI_Layer/HomeView.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using UI_Layer.Resources;
+using UI_Layer.Assets;
 
 namespace UI_Layer
 {
