e9604dd885210f4778e192c27fbe2a4d73b9f1d0
Zenitude <evan.damas.2003@gmail.com>
2023-10-19 09:51:12 +0200

    Création de HomeViewModel

---
+++ SushiGo/UI_Layer/ViewModels/HomeViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_Layer.ViewModels
+{
+    public class HomeViewModels : INotifyPropertyChanged
+    {
+
+        #region Constructeur
+
+        public HomeViewModels()
+        {
+            
+        }
+
+        #endregion Constructeur
+
+        #region Evenement
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        #endregion Evenement
+    }
+}
