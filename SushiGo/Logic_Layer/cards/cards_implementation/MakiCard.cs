0da9fcd8e702d320e52c533c3da26db944148488
Elia Gautier <elia@gautier.pasUnEmail>
2023-11-28 10:35:55 +0100

    Ajout de tests unitaires à la ViewModel du joueur

--- SushiGo/Logic_Layer/cards/cards_implementation/MakiCard.cs
+++ SushiGo/Logic_Layer/cards/cards_implementation/MakiCard.cs
@@ -7,15 +7,6 @@ public class MakiCard : Card
         return quantity == other.quantity;
     }
 
-    public override bool Equals(object? obj)
-    {
-        if (ReferenceEquals(null, obj)) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != this.GetType()) return false;
-        if ((obj as MakiCard)!.Quantity.Equals(Quantity) && (obj as MakiCard)!.Name.Equals(Name)) return true;
-        return Equals((MakiCard)obj);
-    }
-
     public override int GetHashCode()
     {
         return quantity;
