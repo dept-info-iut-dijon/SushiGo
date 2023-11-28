8867bf72920eaec1fa1a99794c946b6588c70380
LeoPisan <92891905+LeoPisan@users.noreply.github.com>
2023-11-28 07:50:39 +0100

    Début d'ajout de tests unitaires aux ViewModels

---
+++ SushiGo/ViewModel_Test/PlayerViewModelTests.cs
@@ -0,0 +1,11 @@
+namespace ViewModel_Test;
+
+public class PlayerViewModelTests
+{
+    [Theory]
+    [InlineData(false, true)]
+    public void IsTurnFinished_GetNotification(bool initValue, bool newValue)
+    {
+        
+    }
+}
