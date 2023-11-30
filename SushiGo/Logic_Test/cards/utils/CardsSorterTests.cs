43408e3e98501b10d9590ce69992ef2e421c9cde
Elia Gautier <elia@gautier.pasUnEmail>
2023-11-30 10:18:25 +0100

    Ajout de TU sur la fonction de tri des cartes par type

---
+++ SushiGo/Logic_Test/cards/utils/CardsSorterTests.cs
@@ -0,0 +1,22 @@
+﻿using Logic_Layer.cards;
+using LogicTest.datas_generators;
+using Logic_Layer.cards.utils;
+
+namespace LogicTest.cards.utils;
+
+public class CardsSorterTests
+{
+    [Theory]
+    [ClassData(typeof(CardsSorterGenerator))]
+    public void TypeSort_ShouldReturnSortedCards(Card[] cards, Card[] expected, Type type)
+    {
+        var actual = new CardsSorter().SortCards(type, cards);
+        Assert.Equal(expected.Length, actual.Length);
+
+        for (int i = 0; i < expected.Length; i++)
+        {
+            Assert.Equal(expected[i].GetType(), actual[i].GetType());
+            Assert.True(expected[i].Equals(actual[i]));
+        }
+    }
+}
