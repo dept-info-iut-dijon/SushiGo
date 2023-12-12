045294027dc5337235e9a3d3e927a62211805ec0
EvanDamas <evan.damas@iut-dijon.u-bourgogne.fr>
2023-12-12 12:01:07 +0100

    Ajout doc

--- SushiGo/UI_Layer/App.xaml.cs
+++ SushiGo/UI_Layer/App.xaml.cs
@@ -6,6 +6,7 @@ namespace Couche_IHM
 {
     public partial class App : Application
     {
+        /// <inheritdoc/>
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
@@ -13,14 +14,13 @@ namespace Couche_IHM
             // Écoute l'événement PreviewMouseDown au niveau de l'application pour toutes les fenêtres
             EventManager.RegisterClassHandler(typeof(Window), UIElement.PreviewMouseDownEvent, new MouseButtonEventHandler(App_PreviewMouseDown));
 
-            // Charge le curseur initial
-            Cursor customCursor = (Cursor)FindResource("MainCursor");
-            Mouse.OverrideCursor = customCursor;
+            // Curseur customisé
+            Mouse.OverrideCursor = (Cursor)FindResource("MainCursor");
         }
 
         private async void App_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            Mouse.OverrideCursor = (Cursor)FindResource("MainCursorOn");
+            Mouse.OverrideCursor = (Cursor)FindResource("MainCursorClick");
             await Task.Delay(300);
             Mouse.OverrideCursor = (Cursor)FindResource("MainCursor");
         }
