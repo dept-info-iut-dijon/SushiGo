7f0a412c9fe5689d2cf5575c7477ee9d29dd3d32
Florian <florian.marteau@iut-dijon.u-bourgogne.fr>
2023-12-13 11:55:45 +0100

    Fix TU

--- SushiGo/ViewModel_Test/GameTableViewModelTests.cs
+++ SushiGo/ViewModel_Test/GameTableViewModelTests.cs
@@ -10,48 +10,6 @@ namespace ViewModel_Test
     public class GameTableViewModelTests
     {
         [Fact]
-        public void ButtonValidateEnable_ShouldBeFalse_WhenCardSelectedIsNull()
-        {
-            // Arrange
-            var viewModel = new GameTableViewModel();
-
-            // Act
-            bool buttonEnable = viewModel.IsButtonValidateShown;
-
-            // Assert
-            Assert.False(buttonEnable);
-        }
-
-        [Fact]
-        public void ButtonValidateEnable_ShouldBeTrue_WhenCardSelectedIsNotNull()
-        {
-            // Act
-            Thread thread = new Thread(() =>
-            {
-                // Arrange
-                var gameTableVM = new GameTableViewModel();
-                Player player = new Player(2, new Board(), new Hand(2, new List<Card>()), "Spartacus");
-                PlayerViewModel playerVM = new PlayerViewModel(player, PlayerType.PLAYER);
-                var card = new CardComponent(playerVM, new ChopstickCard());
-
-                SynchronizationContext.SetSynchronizationContext(new DispatcherSynchronizationContext(Dispatcher.CurrentDispatcher));
-
-                gameTableVM.CardSelected = card;
-                bool buttonEnable = gameTableVM.IsButtonValidateShown;
-
-                // Assert
-                Assert.True(buttonEnable);
-
-                Dispatcher.CurrentDispatcher.BeginInvokeShutdown(DispatcherPriority.Background);
-                Dispatcher.Run();
-            });
-
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
-            thread.Join();
-        }
-
-        [Fact]
         public void CardSelected_ShouldRaisePropertyChangedEvent()
         {
             // Act
