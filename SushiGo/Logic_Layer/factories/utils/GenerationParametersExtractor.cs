2ca745dfee46ba979c3f38fcb750e9da69857d06
Elia Gautier <elia@gautier.pasUnEmail>
2023-12-15 17:18:54 +0100

    Ajout des baguettes au deck

--- SushiGo/Logic_Layer/factories/utils/GenerationParametersExtractor.cs
+++ SushiGo/Logic_Layer/factories/utils/GenerationParametersExtractor.cs
@@ -26,9 +26,9 @@ public class GenerationParametersExtractor
             { "sushi saumon", 10 },
             { "sushi calamar", 5 },
             { "sushi omelette", 10 },
-            { "dessert", 10 }//,
+            { "dessert", 10 },
             // { "wasabi", 6 },
-            // { "baguette", 4 }
+            { "baguette", 4 }
         };
         //TODO La génération des cartes spéciales est désactivée pour l'instant car leur gestion n'est pas encore totalement implémentée
     }
