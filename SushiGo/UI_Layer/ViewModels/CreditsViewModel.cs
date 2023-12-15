d0ef2859ccd41cd250f726c608a6044c5896060e
EvanDamas <evan.damas@iut-dijon.u-bourgogne.fr>
2023-12-15 17:22:15 +0100

    Créationd de la vue des crédits ainsi que sa vueModel

---
+++ SushiGo/UI_Layer/ViewModels/CreditsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UI_Layer.ViewModels
+{
+    /// <summary>
+    /// Classe intermédiaire entre la vue <see cref="CreditsView"/> et le métier.
+    /// </summary>
+    public class CreditsViewModel
+    {
+    }
+}
---
+++ SushiGo/UI_Layer/Views/CreditsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace UI_Layer.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour CreditsView.xaml
+    /// </summary>
+    public partial class CreditsView : Window
+    {
+        public CreditsView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
